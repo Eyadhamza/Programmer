1e7289a2307d9ebb756ec46cb8e9d1d19eda32a4
EyadHamza <EyadHamza0@outlook.com>
2020-09-23 21:51:29 +0200

    first commit

---
+++ app/Http/Controllers/TrackController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TrackCollection;
+use App\Http\Resources\TrackResource;
+use App\Models\Track;
+use Illuminate\Http\Request;
+
+class TrackController extends Controller
+{
+
+    public function index()
+    {
+        $tracks=Track::all();
+        return new TrackCollection($tracks);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        $track=Track::findOrFail($id);
+        return new TrackResource($track);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/factories/TrackFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Track;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TrackFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Track::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->title,
+            'image'=>'https://randomuser.me/api/portraits/med/men/'.$this->faker->numberBetween(0,100).'.jpg',
+            'video_url'=>'url of video',
+
+        ];
+    }
+}
